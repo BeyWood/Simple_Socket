1ae01edfd4408467a2baeec91632996921a13a75
aidaiyan520 <1046991500@qq.com>
2016-06-06 17:00:27 +0800

    Initial Commit

---
+++ HysChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HysChat
+//
+//  Created by Jason Huang on 16/6/6.
+//  Copyright © 2016年 JasonHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HysChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HysChat
+//
+//  Created by Jason Huang on 16/6/6.
+//  Copyright © 2016年 JasonHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
